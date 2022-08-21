8414e96594abeff6f820466230391562fcef9d63
Zahradeen Muazu <Zahradeenmuazu@yahoo.com>
2022-08-21 08:26:35 +0100

    modified graphql

--- src/index.js
+++ src/index.js
@@ -8,18 +8,51 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
+let notes = [
+  {id:'1',content:'This is a note',author:'Adam Scott'},
+  {id:'2',content:'This is another note',author:'Harlow Everly'},
+  {id:'3',content:'Oh hey, look, another note',author:'Riley Harrison'},
+];
+
 //construct a schema using graphql schema language
 const typeDefs = gql`
-  type Query {
-    hello: String
+  type Note {
+    id: ID!
+    content:String!
+    author:String!
+  },
+  type Query{
+    hello:String
+    notes:[Note!]!
+    note(id:ID):Note!
+  }
+  type Mutation {
+    newNote(content:String!):Note!
   }
 `;
+
 
 // Provide resolver functions for schema fields
 const resolvers = {
   Query:{
-    hello: ()=>'Hello World'
+    hello: ()=>'Hello World',
+    notes:()=>notes,
+    note:(parent,args)=>{
+      return notes.find(note=> note.id === args.id);
+    }
+  },
+  Mutation:{
+    newNote:(parent, args)=>{
+      let noteValue = {
+        id:String(notes.length + 1),
+        content : args.content,
+        author:'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue
+    }
   }
+
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
